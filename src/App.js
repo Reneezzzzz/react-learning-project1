637e51a9821832d83ba9340553f698a80ead616b
Reneezzzzz <112042893+Reneezzzzz@users.noreply.github.com>
2023-02-04 16:55:09 +1100

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
-import React from "react";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "./Button";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          secondary
+          outline
+          rounded
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
+
+export default App;
